105e55eeb62ba3f60603652f6556214c6cae83f3
April Coloma <toughweed@gmail.com>
2013-04-16 15:30:08 +0800

    add vcard template

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("Hello World! The Pink Project");
+
+
+var info = {
+    firstName: "Jerome",
+    lastName: "Coloma"
+};
+
+var a = info['firstName'].innerHTML;
+
+
+// var firstNameEl = document.getElementById('first-name');
+
+// firstNameEl.addEventListener('click', function() { 
+//         firstNameEl.innerHTML = "April Modified";
+//     }
+// );
+
+
+
+// console.log(document.getElementsByTagName('div')[1]);
+// console.log(document.getElementsByClassName('vcard-box'));
+
+// var d = window.document;
+// console.log(d.getElementsByTagName('div'));
